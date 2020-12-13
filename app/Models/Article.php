d3cf9c8b05508d037629441f1bea22e82595335a
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2020-12-13 12:28:03 +0100

    [article] use sluggable

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,10 +6,12 @@ use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Article extends Model
 {
-    use CrudTrait, HasFactory, HasTimestamps;
+    use CrudTrait, HasFactory, HasSlug, HasTimestamps;
 
     protected $fillable = [
         'title',
@@ -22,5 +24,23 @@ class Article extends Model
     public function parent()
     {
         return $this->belongsTo(self::class);
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
