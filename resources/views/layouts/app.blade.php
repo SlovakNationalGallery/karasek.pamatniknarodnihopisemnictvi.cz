5669227de8ec1a77948adc3f45ddffe48eb1eb5b
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2020-12-18 10:46:07 +0100

    meta tags, favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffc40d">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta property="og:title" content="Virtuální galerie Jiřího Karáska ze Lvovic" />
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website" />
+    <meta property="og:author" content="http://lab.sng.sk/" />
+    <meta property="og:url" content="https://karasek.pamatniknarodnihopisemnictvi.cz" />
+    <meta property="og:image" content="https://karasek.pamatniknarodnihopisemnictvi.cz/img/og-image.jpg" />
+    <meta property="og:site_name" content="Virtuální galerie Jiřího Karáska ze Lvovic" />
+
     <title>Virtuální galerie Jiřího Karáska ze Lvovic</title>
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
@@ -14,6 +30,5 @@
     </div>
 
     <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
-    @stack('scripts')
 </body>
 </html>
