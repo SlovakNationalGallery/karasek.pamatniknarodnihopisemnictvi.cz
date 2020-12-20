2171722431e9ee3b1b6532dc6a8ceeedd6c9ff60
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2020-12-20 22:05:32 +0100

    [homepage] menu item no max width

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,7 +9,7 @@
         <div class="my-12 relative">
             <ol class="lg:content-between flex flex-col flex-wrap items-center lg:justify-evenly list-decimal list-inside lg:h-168 relative z-10">
                 @foreach($menu as $slug => $title)
-                    <li class="bg-white my-4 lg:my-8 max-w-md p-4 relative text-center transform {{ rand(0, 1) ? 'lg:-rotate-3' : 'lg:rotate-3' }}">
+                    <li class="bg-white my-4 lg:my-8 p-4 relative text-center transform {{ rand(0, 1) ? 'lg:-rotate-3' : 'lg:rotate-3' }}">
                         <a class="whitespace-pre-line" href="{{ route('article', ['article' => $slug]) }}">{{ $title }}</a>
                         <a class="absolute inset-0" href="{{ route('article', ['article' => $slug]) }}"></a>
                     </li>
