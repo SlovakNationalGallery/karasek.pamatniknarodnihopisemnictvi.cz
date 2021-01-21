35fbd05971d3639f2807430119248a1e1f3ad0c3
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-01-21 21:14:08 +0100

    [homepage] redesign

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,24 +1,15 @@
-@extends('layouts.app', ['bodyClasses' => 'bg-cover bg-texture'])
+@extends('layouts.app')
 
 @section('content')
-    <div class="max-w-6xl mx-auto">
-        <h1>Virtuální galerie Jiřího Karáska ze Lvovic</h1>
+    <div class="bg-cover border-2 md:border-4 border-black relative" style="padding-bottom: 29.72%; background-image: url('../img/background.jpg');">
+        <h1 class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 whitespace-nowrap" style="font-size: 6vw;">Virtuální galerie Jiřího Karáska ze Lvovic</h1>
     </div>
 
-    <div class="max-w-screen-2xl mx-auto">
-        <div class="my-12 relative">
-            <ol class="lg:content-between flex flex-col flex-wrap items-center lg:justify-evenly list-decimal list-inside lg:h-168 relative z-10">
-                @foreach($menu as $slug => $title)
-                    <li class="bg-white my-4 lg:my-8 p-4 relative text-center transform {{ rand(0, 1) ? 'lg:-rotate-3' : 'lg:rotate-3' }}">
-                        <a class="whitespace-pre-line" href="{{ route('article', ['article' => $slug]) }}">{{ $title }}</a>
-                        <a class="absolute inset-0" href="{{ route('article', ['article' => $slug]) }}"></a>
-                    </li>
-                @endforeach
-            </ol>
-
-            <div class="absolute left-1/2 mx-auto top-0 transform -translate-x-1/2 w-128 z-0 hidden lg:block">
-                <img class="border-2 md:border-4 border-black" src="/img/karasek.jpg" alt="Jiří Karásek">
-            </div>
+    <div class="md:border-0.5 border-black md:mt-10 p-5 text-center">
+        <div class="max-w-6xl mx-auto">
+            @foreach($menu as $slug => $title)
+                <a class="block md:inline-block font-medium hover:no-underline mx-2 my-1 underline uppercase" href="{{ route('article', ['article' => $slug]) }}">{{ $title }}</a>
+            @endforeach
         </div>
     </div>
 @endsection
