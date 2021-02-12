e75a3a53d9f9ad93c3db568ae7b477bb179c14b4
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-02-12 12:15:56 +0100

    change catalog perex

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -6,10 +6,10 @@
 
         <div class="max-w-4xl mx-auto my-4 lg:my-8">
             <p class="font-medium my-4 lg:my-8 text-center text-xl lg:text-2xl">
-                <span class="font-italic">Katalog výstavy</span> představuje místo, na kterém divák Virtuální
+                Katalog výstavy představuje místo, na kterém divák Virtuální
                 galerie Jiřího Karáska ze Lvovic nalezne všechna dosud digitálně
                 zpracovaná díla Karáskovy sbírky a kde mezi nimi může procházet
-                jak na základě jejich názvů, námětů, tak podle data jejich vzniku.
+                jak na základě jejich autorů, námětů, tak podle data jejich vzniku.
             </p>
         </div>
 
